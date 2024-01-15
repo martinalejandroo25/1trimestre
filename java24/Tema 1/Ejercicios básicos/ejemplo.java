44d3ccca2558cadf8331fe8242cc14f9076fb63a
martinalejandroo25 <martinalejandroo25@gmail.com>
2024-01-15 01:45:17 +0100

    xd

--- java24/Tema 1/Ejercicios básicos/ejemplo.java
+++ java24/Tema 1/Ejercicios básicos/ejemplo.java
@@ -1,9 +1,9 @@
 public class ejemplo {
     
-public static void main(String[] args) {
-   
-    System.out.println("hola mundo");
+    public static void main(String[] args) {
+    
+        System.out.println("hola mundo");
 
-}
+    }
 
 }
